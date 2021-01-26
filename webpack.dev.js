72ecdf89f606a6fb90a76b18bacea7beb68522be
yuhashmi <giogiosef97@gmail.com>
2021-01-26 20:14:40 +0000

    changed file name references

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/template.html',
+      template: './src/Index.html',
       favicon: './src/assets/favicon.png'
     })
   ],
